8af55f26e5534afeb89c6d345c727001732042bf
stackasaur <ethan@stackasaur.us>
2026-06-09 10:21:55 -0400

    change dir

---
+++ cliox_macros/src/lib.rs
@@ -0,0 +1,13 @@
+/**
+ * cliox provides the Commandable trait and cliox attribute to define how a struct
+ * can be turned into a std::process::Command
+ */
+extern crate proc_macro2;
+
+mod parser;
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(Commandable, attributes(cliox))]
+pub fn derive_commandable(item: TokenStream) -> TokenStream {
+    parser::derive_cliox_impl(item)
+}
